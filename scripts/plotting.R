dfaa3b53f7d2ae3b6337c86a00543e6e0f9ea781
cmt2 <cmt2@berkeley.edu>
2021-02-22 11:26:34 -1000

    updating scripts

--- scripts/plotting.R
+++ scripts/plotting.R
@@ -1,11 +1,43 @@
 library(RevGadgets)
 library(coda)
-t <- readTrace(path = "~/Documents/carex_chromo/output/tp_dirichlet.log")
-trace <- t[[1]]
+library(ggpubr)
+library(gridExtra)
+library(grid)
 
-ess <- effectiveSize(as.mcmc(trace))
+t1 <- readTrace(paths = "~/Documents/carex_chromo/output/tp_dirichlet_withoutSiderosticta_run1.log")
+t2 <- readTrace(paths = "~/Documents/carex_chromo/output/tp_dirichlet_withoutSiderosticta_run2.log")
 
-pdf("~/Documents/carex_chromo/convergence_orig.pdf")
+trace1 <- t1[[1]]
+trace2 <- t2[[1]]
+
+tracec <- rbind(removeBurnin(t1, 0.1)[[1]], removeBurnin(t2, 0.1)[[1]])
+tc <- list(tracec)
+
+ess1 <- effectiveSize(as.mcmc(trace1))
+ess2 <- effectiveSize(as.mcmc(trace2))
+essc <- effectiveSize(as.mcmc(tracec))
+
+t1 <- removeBurnin(t1, 0.1)
+t2 <- removeBurnin(t2, 0.1)
+
+pdf("~/Documents/carex_chromo/convergence_withoutSiderosticta2.pdf")
+trace <- trace2
+ess <- ess2
+t <- t2
+
+sum <- summarizeTrace(t, vars = c("clado_fission","clado_fusion",
+                                  "clado_no_change")) 
+sum_df <- data.frame(mean = c(round(sum[[1]][[1]]$mean, digits = 3), 
+                              round(sum[[2]][[1]]$mean, digits = 3), 
+                              round(sum[[3]][[1]]$mean, digits = 3)), 
+                     quantile_2.5 = c(round(sum[[1]][[1]]$quantile_2.5, digits = 3),
+                                      round(sum[[2]][[1]]$quantile_2.5, digits = 3),
+                                      round(sum[[3]][[1]]$quantile_2.5, digits = 3)), 
+                     quantile_97.5 = c(round(sum[[1]][[1]]$quantile_97.5, digits = 3),
+                                       round(sum[[2]][[1]]$quantile_97.5, digits = 3),
+                                       round(sum[[3]][[1]]$quantile_97.5, digits = 3)))
+row.names(sum_df) <- names(sum)
+
 par(mfrow=c(3,1))
 plot(trace$clado_fission, 
      type = "l", 
@@ -27,12 +59,19 @@ plot(trace$clado_no_change,
      main = paste0("Clado_no_change ESS: ", round(ess["clado_no_change"], digits = 2)))
 
 par(mfrow=c(1,1))
-plotTrace(removeBurnin(t, 0.1), vars = c("clado_fission","clado_fusion",
+
+plotTrace(t, vars = c("clado_fission","clado_fusion",
                                              "clado_no_change"))
+grid.newpage()
+grid.table(sum_df)
+
 
 t[[1]]$diff_fission_nochange <- t[[1]]$clado_fission-t[[1]]$clado_no_change
 t[[1]]$diff_fusion_nochange <- t[[1]]$clado_fusion-t[[1]]$clado_no_change
 
-plotTrace(removeBurnin(t, 0.1),
+
+plotTrace(t,
           vars = c("diff_fission_nochange","diff_fusion_nochange"))
+
+
 dev.off()
