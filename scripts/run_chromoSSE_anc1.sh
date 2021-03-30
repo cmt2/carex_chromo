f5d670dc9b95976611223c25541847e57c000cff
cmt2 <cmt2@berkeley.edu>
2021-03-29 15:53:52 -1000

    updating anc states and adding slurm files

---
+++ scripts/run_chromoSSE_anc1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=chromoSSE_anc_states
+#SBATCH --time=00-00:30:00
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=8G
+#SBATCH --account=zenilab
+#SBATCH --partition=zenilab
+#SBATCH --ntasks=1
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user=cmt2@hawaii.edu
+
+
+module load devel/Boost/1.69.0-GCCcore-6.3.0
+module load toolchain/intel/2018.5.274
+
+./rb scripts/tp_ChromoSSE_carex_dirichlet_withSiderosticta_anc_states1.Rev
