f82ec63e8dac7ea12a9e589e74ee096c6b294724
zenillab <zenillab@dhcp-168-105-84-58.wireless.manoa.hawaii.edu>
2021-08-16 14:00:15 -1000

    updating chromohisse code

--- data/transformation_for_hiddenstates.R
+++ data/transformation_for_hiddenstates.R
@@ -1,9 +1,9 @@
 ### Transformation of the data to expand to test hidden states model for chromosome number
 
-carex_data<-read.table("~/data/carex_chromosome_counts_withoutSiderosticta.tsv")
+carex_data<-read.table("data/carex_chromosome_counts_withoutSiderosticta.tsv")
 maximum<-62+10 # maximum number of chromosomes is 62 and we go with 10 more (Mayrose et al. 2010 recommendation)
 second_column<-as.numeric(carex_data[,2]) 
-expand_data<- second_column+maximum # Expansion
+expand_data<- second_column+maximum+1 # Expansion (leave room for '0' in hidden state )
 
 #Creating the double entry in each datum
 new_column<- paste0("(", second_column, " ",expand_data,")")
@@ -12,4 +12,7 @@ carex_new_data<-data.frame(new_column)
 
 
 ### Writing the new table
-write.table(carex_new_data, file="~/data/carex_chromosome_counts_expanded_withoutSiderosticta.tsv", sep="\t")
+write.table(carex_new_data, 
+            file="data/carex_chromosome_counts_expanded_withoutSiderosticta.tsv", 
+            sep="\t",
+            col.names = FALSE)
